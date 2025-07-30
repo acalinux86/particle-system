5cf2dbbb9ac37475bcfa87bfdcdf5a27b9869091
acalinux86 <aacali6777@gmail.com>
2025-07-30 18:44:28 +0300

    Successfully Implement World to Screen Coordinates

--- utils/matvec.c
+++ utils/matvec.c
@@ -1,5 +1,15 @@
 #include "./matvec.h"
 
+Vector4 v4_from_v3(Vector3 vec3)
+{
+    return v4_init(vec3.x, vec3.y, vec3.z, 1.0f);
+}
+
+Vector3 v3_from_v4(Vector4 vec4)
+{
+    return v3_init(vec4.x, vec4.y, vec4.z);
+}
+
 Vector4 mv4_transform(Matrix4 mat_4, Vector4 vec_4)
 {
     /* return v4_init(
@@ -8,7 +18,7 @@ Vector4 mv4_transform(Matrix4 mat_4, Vector4 vec_4)
         ((mat_4.row3[0] * vec_4.x) + (mat_4.row3[1] * vec_4.y) + (mat_4.row3[2] * vec_4.z) + (mat_4.row3[3] * vec_4.w)),
         ((mat_4.row4[0] * vec_4.x) + (mat_4.row4[1] * vec_4.y) + (mat_4.row4[2] * vec_4.z) + (mat_4.row4[3] * vec_4.w))
     );*/
-    
+
     float v4[MAT4_ROWS] = {0};
     for (uint32_t i = 0; i < MAT4_ROWS; ++i) {
         v4[i] = ((mat_4.rows[i][0] * vec_4.x) +
@@ -32,4 +42,23 @@ Matrix4 mv4_translate(Vector4 vec_4)
         }
     };
     return result;
+}
+
+Vector3 to_screen_coords(Vector3 vec3, float screen_width, float screen_height)
+{
+    Vector4 vec4 = v4_from_v3(vec3);
+    V4_PRINT(vec4);
+
+    Matrix4 ModelMatrix = {
+        .rows = {
+            {screen_width/2.0f, 0.0f, 0.0f, screen_width/2.0f},
+            {0.0f, -screen_height/2.0f, 0.0f, screen_height/2.0f},
+            {0.0f, 0.0f, 1.0f, 0.0f},
+            {0.0f, 0.0f, 0.0f, 1.0f}
+        }
+    };
+    Vector4 TransformedVec4 = mv4_transform(ModelMatrix, vec4);
+    V4_PRINT(TransformedVec4);
+
+    return v3_from_v4(TransformedVec4);
 }
